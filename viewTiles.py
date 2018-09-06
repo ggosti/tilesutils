bdfb1591ab3c4c4e02067b402393c0339c42ce73
Giorgio Gosti <giogosti@gmail.com>
2018-09-06 09:34:05 +0000

    viewTiles.py edited imporved arguments, added comments

--- viewTiles.py
+++ viewTiles.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.widgets import Slider, Button, RadioButtons
 from skimage import io
 import os
 import glob
 import cv2
 import sys
 import pandas as pd
+import argparse
+
 
 def nothing(x):
     pass
@@ -48,17 +51,38 @@ def getImName(autofocus,fileName,x,y):
         try:
             z = int(find_between( pathIm, 'z','.tif' ))
         except ValueError:
-            z = int(find_between( pathIm, 'z','delta' ))
+            try: 
+                z = int(find_between( pathIm, 'z','delta' ))
+            except ValueError:
+                z = int(find_between( pathIm, 'z','tiles' ))
     return pathIm,z
 
 autofocus = False
-doTile = True
+#doTile = True
+#show = True
 
-dirPics = sys.argv[1]
-print "Name of dir: ",  dirPics
-if len(sys.argv)>2:
-    doTile = (sys.argv[2] == 'True')
-    print "Do Tile: ",  doTile
+parser = argparse.ArgumentParser(description='Process mosaic of z-stacks tails.')
+parser.add_argument('-l', type=str,
+                    help='directory with tiles')
+parser.add_argument('--doTile', help='Generate mosaic with tiles', action='store_true')
+parser.add_argument('--show', help='Check and modify parameters', action='store_true')
+args = parser.parse_args()
+
+print args
+doTile = args.doTile
+show = args.show
+dirPics = args.l
+
+#dirPics = sys.argv[1]
+#print "Name of dir: ",  dirPics
+#if len(sys.argv)>2:
+#    doTile = (sys.argv[2] == 'True')
+#    if (sys.argv[2] == '-noShow'):
+#        doTile = True
+#        show = False
+print "Do show: ",  show
+print "Do Tile: ",  doTile
+print dirPics
 
 os.chdir(dirPics)
 for fileName in glob.glob('*.scan'):
@@ -97,6 +121,9 @@ def zFun(x,y,x0,y0,z0,a,b,zmin,zmax):
     z = np.min([z,zmax-1])
     return z
 
+#####################################################
+# Check focus pparameters
+#########################################################
 
 if os.path.isfile(cwd+fileName+'-focusPoins.csv'):
     df = pd.read_csv(cwd+fileName+'-focusPoins.csv')
@@ -132,138 +159,121 @@ else:
         minzs[i-1] = minz
         zs[i-1] = minz + zs[i-1]
         print iName,minz
+a = float(zs[2]-zs[0])/(xs[2]-xs[0]) #
+b = float(zs[1]-zs[0])/(ys[1]-ys[0])
 print('Points x', xs,'y',ys,'z',zs,'iNames',iNames,'min z',minzs)
 
-chx,chy = 9,9
-ans = raw_input('Set check point different from: ('+str(chx)+','+str(chy)+') ? (y/n)')
-ans = ans.split(',')
-print ans
-if ans[0] == 'y':
-    chx = int(raw_input('Enter your point '+str(i)+' x: '))
-    chy = int(raw_input('Enter your point '+str(i)+' y: '))
-elif len(ans) == 2:
-    chx,chy = int(ans[0]),int(ans[-1])
-print('Check Point ',chx,chy)
-
-
+##############################################################
+# Check focus parameters
+####################################################################
 
 pathIm1 = iNames[0]
 im1 = io.imread(pathIm1)
-print 'Point1 : ',pathIm1
+print
+'Point1 : ', pathIm1
 
-pathIm2 = iNames[1]
-im2 = io.imread(pathIm2)
-print 'Point2 : ',pathIm2
+if show:
+    chx, chy = 9, 9
+    ans = raw_input('Set check point different from: (' + str(chx) + ',' + str(chy) + ') ? (y/n)')
+    ans = ans.split(',')
+    print
+    ans
+    if ans[0] == 'y':
+        chx = int(raw_input('Enter your point ' + str(i) + ' x: '))
+        chy = int(raw_input('Enter your point ' + str(i) + ' y: '))
+    elif len(ans) == 2:
+        chx, chy = int(ans[0]), int(ans[-1])
+    print('Check Point ', chx, chy)
 
-pathIm3 = iNames[2]
-im3 = io.imread(pathIm3)
-print 'Point3 : ',pathIm3
+    pathIm2 = iNames[1]
+    im2 = io.imread(pathIm2)
+    print 'Point2 : ',pathIm2
 
-
-pathImC, zCh = getImName(autofocus,fileName,chx,chy)
-imC = io.imread(pathImC)
-print 'Check Point : ',pathImC,zCh
-
-print('zstack size',im1.shape[0],im2.shape[0],im3.shape[0]) 
-
-
-zBase1,zBase2,zBase3= minzs
-
-cv2.namedWindow('Point1', cv2.WINDOW_NORMAL)
-cv2.createTrackbar('Z', 'Point1', zs[0]-zBase1, im1.shape[0]-1, nothing)
-cv2.namedWindow('Point2', cv2.WINDOW_NORMAL)
-cv2.createTrackbar('Z', 'Point2', zs[1]-zBase2, im2.shape[0]-1, nothing)
-cv2.namedWindow('Point3', cv2.WINDOW_NORMAL)
-cv2.createTrackbar('Z', 'Point3', zs[2]-zBase3, im3.shape[0]-1, nothing)
-cv2.namedWindow('Check', cv2.WINDOW_NORMAL)
-#cv2.createTrackbar('Z', 'Check', zcheck, imC.shape[0], nothing)
-
-# Do whatever you want with contours
-imMin1 = im1.min()
-imMax1 = im1.max()
-scale1 = 255.0/float(imMax1-imMin1)#float(mii-1)
-print(((im1-imMin1)*scale1).max() )
-imMin2 = im2.min()
-imMax2 = im2.max()
-scale2 = 255.0/float(imMax2-imMin2)#float(mii-1)
-imMin3 = im3.min()
-imMax3 = im3.max()
-scale3 = 255.0/float(imMax3-imMin3)#float(mii-1)
+    pathIm3 = iNames[2]
+    im3 = io.imread(pathIm3)
+    print 'Point3 : ',pathIm3
 
 
-imMinC = imC.min()
-imMaxC = imC.max()
-scaleC = 255.0/float(imMaxC-imMinC)
-zOld = [0,0,0,0]
-font = cv2.FONT_HERSHEY_SIMPLEX
+    pathImC, zCh = getImName(autofocus,fileName,chx,chy)
+    imC = io.imread(pathImC)
+    print 'Check Point : ',pathImC,zCh
 
-def showTile(wName,im,zval,zBase,imMin,scale,font=font):
-    img = np.uint8((im[zval-zBase,:,:]-imMin)*scale)
-    cv2.putText(img,str(zval),(10,200), font, 4,(255,255,255),2,cv2.LINE_AA)
-    cv2.imshow(wName, img)    
+    print('zstack size',im1.shape[0],im2.shape[0],im3.shape[0])
 
-while(1):
-    a = float(zs[2]-zs[0])/(xs[2]-xs[0]) #
-    b = float(zs[1]-zs[0])/(ys[1]-ys[0])
-    zcheck = zFun(chx,chy,xs[0],ys[0],zs[0],a,b,zCh,zCh+imC.shape[0])
-    #print 'zs',zs,zcheck
-    zval1 = cv2.getTrackbarPos('Z','Point1') + zBase1
-    showTile('Point1',im1,zval1,zBase1,imMin1,scale1)
-    zval2 = cv2.getTrackbarPos('Z','Point2') + zBase2
-    showTile('Point2',im2,zval2,zBase2,imMin2,scale2)
-    #cv2.imshow('Point2', np.uint8((im2[zval2-zBase2,:,:]-imMin2)*scale2) )
-    zval3 = cv2.getTrackbarPos('Z','Point3') + zBase3
-    showTile('Point3',im3,zval3,zBase3,imMin3,scale3)
-    #cv2.imshow('Point3', np.uint8((im3[zval3-zBase3,:,:]-imMin3)*scale3) )
-    showTile('Check',imC,zcheck,zCh,imMinC,scaleC)
-    k = cv2.waitKey(1) & 0xFF
-    zs = [zval1,zval2,zval3]
-    if not (np.array(zOld) == np.array(zs + [zcheck])).all():
-        print zval1,zval2,zval3
-        print zcheck,zcheck-zCh
-        zOld = zs + [zcheck]
-    if k == 27:  
-        break
-cv2.destroyAllWindows()
+
+    zBase1,zBase2,zBase3= minzs
+
+    cv2.namedWindow('Point1', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('Z', 'Point1', zs[0]-zBase1, im1.shape[0]-1, nothing)
+    cv2.namedWindow('Point2', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('Z', 'Point2', zs[1]-zBase2, im2.shape[0]-1, nothing)
+    cv2.namedWindow('Point3', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('Z', 'Point3', zs[2]-zBase3, im3.shape[0]-1, nothing)
+    cv2.namedWindow('Check', cv2.WINDOW_NORMAL)
+    #cv2.createTrackbar('Z', 'Check', zcheck, imC.shape[0], nothing)
+
+    # Do whatever you want with contours
+    imMin1 = im1.min()
+    imMax1 = im1.max()
+    scale1 = 255.0/float(imMax1-imMin1)#float(mii-1)
+    print(((im1-imMin1)*scale1).max() )
+    imMin2 = im2.min()
+    imMax2 = im2.max()
+    scale2 = 255.0/float(imMax2-imMin2)#float(mii-1)
+    imMin3 = im3.min()
+    imMax3 = im3.max()
+    scale3 = 255.0/float(imMax3-imMin3)#float(mii-1)
+
+
+    imMinC = imC.min()
+    imMaxC = imC.max()
+    scaleC = 255.0/float(imMaxC-imMinC)
+    zOld = [0,0,0,0]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    def showTile(wName,im,zval,zBase,imMin,scale,font=font):
+        img = np.uint8((im[zval-zBase,:,:]-imMin)*scale)
+        cv2.putText(img,str(zval),(10,200), font, 4,(255,255,255),2,cv2.CV_AA)
+        cv2.imshow(wName, img)
+
+    while(1):
+        a = float(zs[2]-zs[0])/(xs[2]-xs[0]) #
+        b = float(zs[1]-zs[0])/(ys[1]-ys[0])
+        zcheck = zFun(chx,chy,xs[0],ys[0],zs[0],a,b,zCh,zCh+imC.shape[0])
+        #print 'zs',zs,zcheck
+        zval1 = cv2.getTrackbarPos('Z','Point1') + zBase1
+        showTile('Point1',im1,zval1,zBase1,imMin1,scale1)
+        zval2 = cv2.getTrackbarPos('Z','Point2') + zBase2
+        showTile('Point2',im2,zval2,zBase2,imMin2,scale2)
+        #cv2.imshow('Point2', np.uint8((im2[zval2-zBase2,:,:]-imMin2)*scale2) )
+        zval3 = cv2.getTrackbarPos('Z','Point3') + zBase3
+        showTile('Point3',im3,zval3,zBase3,imMin3,scale3)
+        #cv2.imshow('Point3', np.uint8((im3[zval3-zBase3,:,:]-imMin3)*scale3) )
+        showTile('Check',imC,zcheck,zCh,imMinC,scaleC)
+        k = cv2.waitKey(1) & 0xFF
+        zs = [zval1,zval2,zval3]
+        if not (np.array(zOld) == np.array(zs + [zcheck])).all():
+            print zval1,zval2,zval3
+            print zcheck,zcheck-zCh
+            zOld = zs + [zcheck]
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
     
-print xs,ys,zs
-df = pd.DataFrame(
-    {'img. name':iNames,
-     'x': xs,
-     'y': ys,
-     'z': zs,
-     'min z': minzs
-    })
+    print xs,ys,zs
+    df = pd.DataFrame(
+        {'img. name':iNames,
+         'x': xs,
+         'y': ys,
+         'z': zs,
+         'min z': minzs
+        })
 
-df.to_csv(cwd+fileName+'-focusPoins.csv')
+    df.to_csv(cwd+fileName+'-focusPoins.csv')
 
-
-numTRows = 3#maxRs
-numTCols= 3#maxCs
-numIRows = im1.shape[1]
-numICols = im1.shape[2]
-print 'Img. shape ',numIRows, numICols
-#tileS = [[np.zeros( (numICols,numIRows) )] * numTRows for ncols in range(numTCols)]
-indices = [[(-1,-1)] * numTCols for nrows in range(numTRows)]
-tileS = [[np.zeros( (numICols,numIRows) )] * numTCols for nrows in range(numTRows)]
-#print tileS
-print len(tileS)
-
-for r in range(1,numTRows+1):
-    print r, numIRows*(r-1),numIRows*(r)
-    for c in range(1,numTCols+1):
-        #print c, numICols*(c-1),numICols*(c)
-        iName,minz = getImName(autofocus,fileName,c+3,r+3)
-        im = io.imread(iName)#cwd+fileName+'x'+str(c)+'y'+str(r)+'.tif')
-        zVal = zFun(c,r,xs[0],ys[0],zs[0],a,b,minz,minz+im.shape[0])
-        #plt.figure()
-        #plt.imshow(im[int(zs),:,:])
-        tileS[r-1][c-1] = im[zVal-minz,:,:]        
-        indices[r-1][c-1] = (r,c)
-
-print indices
-#print tileS
-
+###################################################
+# Load tiles parameters
+###################################################
 
 if os.path.isfile(cwd+fileName+'-tilePars.csv'):
     df = pd.read_csv(cwd+fileName+'-tilePars.csv')
@@ -276,10 +286,6 @@ else:
     angle = 0.6
     overlap = 0.10#0.08
     lres = 1.0/1.0
-tcols = int(numICols*numTCols*lres)
-trows = int(numIRows*numTRows*lres)  
-tile = np.zeros((trows+100,tcols+100),dtype=np.uint16)
-print('tile size ',tile.shape)
 
 
 def traAndRot(im,dx,dy,angle,w,h):
@@ -333,52 +339,91 @@ def stich(tile,tileS,overlap,lres,numTRows,numIRows,numTCols,numICols,angle):
             #plt.imshow(tile)
             #plt.show()
 
-cv2.namedWindow('Tile', cv2.WINDOW_NORMAL)
-percoverlap = int(overlap*100.0)
-angleTbarVal = int(angle*10.0) + 10
-#print 'ov %',percoverlap
-cv2.createTrackbar('Overlap %', 'Tile', percoverlap , 15, nothing)
-cv2.createTrackbar('Rotate deg*10 - 10', 'Tile', angleTbarVal , 20, nothing)
-#percoverlap = cv2.getTrackbarPos('Overlap %','Tile') 
-#print 'ov1 %',percoverlap
+####################################################################
+# check tile parameters
+#################################################################
 
-ovOld = 0
-ovAng = -100.0
-while(1):
-    percoverlap = cv2.getTrackbarPos('Overlap %','Tile') 
-    angleTbarVal = cv2.getTrackbarPos('Rotate deg*10 - 10','Tile') 
+if show:
+    numTRows = 3#maxRs
+    numTCols= 3#maxCs
+    numIRows = im1.shape[1]
+    numICols = im1.shape[2]
+    tcols = int(numICols*numTCols*lres)
+    trows = int(numIRows*numTRows*lres)
+    tile = np.zeros((trows+100,tcols+100),dtype=np.uint16)
+    print('tile size ',tile.shape)
+    print 'Img. shape ',numIRows, numICols
+    #tileS = [[np.zeros( (numICols,numIRows) )] * numTRows for ncols in range(numTCols)]
+    indices = [[(-1,-1)] * numTCols for nrows in range(numTRows)]
+    tileS = [[np.zeros( (numICols,numIRows) )] * numTCols for nrows in range(numTRows)]
+    #print tileS
+    print len(tileS)
+
+    for r in range(1,numTRows+1):
+        print r, numIRows*(r-1),numIRows*(r)
+        for c in range(1,numTCols+1):
+            #print c, numICols*(c-1),numICols*(c)
+            iName,minz = getImName(autofocus,fileName,c+3,r+3)
+            im = io.imread(iName)#cwd+fileName+'x'+str(c)+'y'+str(r)+'.tif')
+            zVal = zFun(c,r,xs[0],ys[0],zs[0],a,b,minz,minz+im.shape[0])
+            #plt.figure()
+            #plt.imshow(im[int(zs),:,:])
+            tileS[r-1][c-1] = im[zVal-minz,:,:]
+            indices[r-1][c-1] = (r,c)
+
+    print indices
+    #print tileS
+
+    cv2.namedWindow('Tile', cv2.WINDOW_NORMAL)
+    percoverlap = int(overlap*100.0)
+    angleTbarVal = int(angle*10.0) + 10
     #print 'ov %',percoverlap
-    overlap = float( percoverlap ) /100.0   
-    angle = float(angleTbarVal -10 ) / 10.0
-    if (not ovOld == overlap) or (not ovAng == angle):
-        print 'new overlap',overlap
-        print 'new angle',angle
-        ovOld = overlap
-        ovAng = angle
-        #print overlap
-        tcols = int(numICols*numTCols*lres)
-        trows = int(numIRows*numTRows*lres)  
-        tile = np.zeros((trows+100,tcols+100))
-        stich(tile,tileS,overlap,lres,numTRows,numIRows,numTCols,numICols,angle)
-        imMinTile = tile.min()
-        imMaxTile = tile.max()
-        #print 'imMinTile',imMinTile,imMaxTile
-        scaleTile = 255.0/float(imMaxTile-imMinTile)
-    cv2.imshow('Tile', np.uint8((tile-imMinTile)*scaleTile) )
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:  
-        break
-cv2.destroyAllWindows()
+    cv2.createTrackbar('Overlap %', 'Tile', percoverlap , 15, nothing)
+    cv2.createTrackbar('Rotate deg*10 - 10', 'Tile', angleTbarVal , 20, nothing)
+    #percoverlap = cv2.getTrackbarPos('Overlap %','Tile')
+    #print 'ov1 %',percoverlap
+
+    ovOld = 0
+    ovAng = -100.0
+    while(1):
+        percoverlap = cv2.getTrackbarPos('Overlap %','Tile')
+        angleTbarVal = cv2.getTrackbarPos('Rotate deg*10 - 10','Tile')
+        #print 'ov %',percoverlap
+        overlap = float( percoverlap ) /100.0
+        angle = float(angleTbarVal -10 ) / 10.0
+        if (not ovOld == overlap) or (not ovAng == angle):
+            print 'new overlap',overlap
+            print 'new angle',angle
+            ovOld = overlap
+            ovAng = angle
+            #print overlap
+            tcols = int(numICols*numTCols*lres)
+            trows = int(numIRows*numTRows*lres)
+            tile = np.zeros((trows+100,tcols+100))
+            stich(tile,tileS,overlap,lres,numTRows,numIRows,numTCols,numICols,angle)
+            imMinTile = tile.min()
+            imMaxTile = tile.max()
+            #print 'imMinTile',imMinTile,imMaxTile
+            scaleTile = 255.0/float(imMaxTile-imMinTile)
+        cv2.imshow('Tile', np.uint8((tile-imMinTile)*scaleTile) )
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
 
 
-print overlap,angle
-df2 = pd.DataFrame(
-    {'overlap': [overlap],
-     'angle': [angle],
-     'lres':[lres]
-    })
+    print overlap,angle
+    df2 = pd.DataFrame(
+        {'overlap': [overlap],
+         'angle': [angle],
+         'lres':[lres]
+        })
 
-df2.to_csv(cwd+fileName+'-tilePars.csv')
+    df2.to_csv(cwd+fileName+'-tilePars.csv')
+
+########################################
+# make tiles
+##########################################
 
 print 'doTile',doTile
 if doTile:
